a2c111a1cbbe2e3e397b03db730b045a53294ea2
Grigore Stefan <g_stefan@yahoo.com>
2017-09-25 00:35:17 +0300

    update

---
+++ component/xui/form-file.php
@@ -0,0 +1,31 @@
+<?php
+//
+// Copyright (c) 2017 Grigore Stefan, <g_stefan@yahoo.com>
+// Created by Grigore Stefan <g_stefan@yahoo.com>
+//
+// The MIT License (MIT) <http://opensource.org/licenses/MIT>
+//
+
+defined('XYO_CLOUD') or die('Access is denied');
+
+$element = $this->getArgument("element");
+
+?>
+<label class="xui-form-label<?php if($this->isElementError($element)){echo " xui-form-label--danger";}; ?>" for="<?php $this->eElementId($element); ?>"><?php $this->eLanguage("label_" . $element); ?><?php if($this->isElementError($element)){echo " - "; $this->eElementError($element);}; ?></label>
+<br>
+<div class="xui-form-file">
+<input type="file" name="<?php $this->eElementName($element); ?>" id="<?php $this->eElementId($element); ?>" class="xui-form-file__file"></input>
+<label for="<?php $this->eElementId($element); ?>"><i class="material-icons">file_upload</i> Browse ...</label><!--
+--><button type="button" class="xui-form-button-icon xui-form-button-icon--info" onclick="document.getElementById('<?php $this->eElementId($element); ?>').value=null;$('#<?php $this->eElementId($element); ?>').trigger('change');"><i class="material-icons">delete</i></button>
+</div>
+<input type="hidden"
+	name="<?php $this->eElementName($element); ?>_delete"
+	value="0"
+	id="<?php $this->eElementId($element); ?>_delete" ></input>
+<input type="hidden"
+	name="<?php $this->eElementName($element); ?>_value"
+	value="<?php $this->eElementValue($element); ?>" ></input>
+<input type="hidden"
+	name="<?php $this->eElementName($element); ?>_file"
+	value="<?php $this->eElementValue($element); ?>" ></input>
+<div class="xui-separator"></div>
---
+++ component/xui/form-file.process.php
@@ -0,0 +1,86 @@
+<?php
+//
+// Copyright (c) 2017 Grigore Stefan, <g_stefan@yahoo.com>
+// Created by Grigore Stefan <g_stefan@yahoo.com>
+//
+// The MIT License (MIT) <http://opensource.org/licenses/MIT>
+//
+
+defined('XYO_CLOUD') or die('Access is denied');
+
+$element = $this->getArgument("element");
+
+$fileName = $this->getArgument("filename","");
+$extension = $this->getArgument("extension",false);
+$deleteBeforeSave = $this->getArgument("delete_before_save",false);
+$elementName = $this->getElementName($element);
+$isOk=false;
+if(strlen($fileName)){
+	if(array_key_exists($elementName,$_FILES)){
+		if(1*$_FILES[$elementName]["error"]){
+			if(strlen($_FILES[$elementName]["name"])){
+				$this->setElementError($element, $this->getFromLanguage("el_".$element."_file_upload_error"));
+			};
+		}else{
+			if($extension){
+				$fileName.=".".strtolower(pathinfo($_FILES[$elementName]["name"], PATHINFO_EXTENSION));
+			};
+			if (move_uploaded_file($_FILES[$elementName]["tmp_name"], $fileName)) {
+				if($deleteBeforeSave){
+					$fileDelete=$this->getElementValue($element."_file","");
+					if(strlen($fileDelete)){
+						$readonly=$this->getArgument("readonly",array());
+						$toDel=true;
+						foreach($readonly as $key=>$value){
+							if($value===$fileDelete){
+								$toDel=false;
+								break;
+							};
+						};
+						if($fileName===$fileDelete){
+							$toDel=false;
+						};
+						if($toDel){
+							if(file_exists($fileDelete)){
+								@unlink($fileDelete);
+							};
+						};
+					};
+				};
+				$this->setElementValue($element."_delete",0);
+				$isOk=true;
+			} else {
+				$this->setElementError($element, $this->getFromLanguage("el_".$element."_file_upload_move_error"));
+			};
+		};
+	};
+};
+
+$isDeleted=false;
+$elementDelete = $this->getElementValue($element."_delete");
+if(strlen($elementDelete)){
+	if(1*$elementDelete==1){
+		$fileDelete=$this->getElementValue($element."_file","");
+		$readonly=$this->getArgument("readonly",array());
+		$toDel=true;
+		foreach($readonly as $key=>$value){
+			if($value===$fileDelete){
+				$toDel=false;
+				break;
+			};
+		};
+		if($toDel){
+			if(file_exists($fileDelete)){
+				@unlink($fileDelete);
+			};
+		};
+		$this->setElementValue($element,"");
+		$isOk=true;
+		$isDeleted=true;
+	};
+};
+
+if(!$isOk){
+	$value=$this->getElementValue($element."_value","");
+	$this->setElementValue($element,$value);
+};
