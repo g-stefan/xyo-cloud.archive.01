38b057787726f28bf8b8599771c25492d5ce1faa
Grigore Stefan <g_stefan@yahoo.com>
2018-03-23 17:51:49 +0200

    update

--- module/xyo-app/xyo-app-logout/module.php
+++ module/xyo-app/xyo-app-logout/module.php
@@ -13,16 +13,18 @@ $className="xyo_app_Logout";
 class xyo_app_Logout extends xyo_Module{
         function __construct(&$object,&$cloud){
                 parent::__construct($object,$cloud);
-                $user=&$this->cloud->getModule("xyo-mod-ds-user");
-                if($user){
-                    $user->makeResetCookie();
-                    $user->doLogout();		   
-                }
-                if($this->isApplication($this->name)){
-                    $this->moduleDisable();
-                    $this->clearRequest();
-		    $this->redirectApplication("xyo-app-login");
-                    return;
-                }
+		if($this->isApplication($this->name)){		
+	                $user=&$this->cloud->getModule("xyo-mod-ds-user");
+        	        if($user){
+                	    $user->makeResetCookie();
+	                    $user->doLogout();		   
+        	        };
+                	if($this->isApplication($this->name)){
+	                    $this->moduleDisable();
+        	            $this->clearRequest();
+			    $this->redirectApplication("xyo-app-login");
+	                    return;
+                	};
+		};
         }
 }
