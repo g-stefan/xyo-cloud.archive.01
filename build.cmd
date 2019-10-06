0f4a1aa9e349df02d5318db117a2991092739258
Grigore Stefan <g_stefan@yahoo.com>
2019-10-06 14:18:25 +0300

    Update

--- build.cmd
+++ build.cmd
@@ -5,6 +5,6 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set ACTION=%1
 if "%1" == "" set ACTION=release
-echo ^> %ACTION% xyo-cloud
+echo -^> %ACTION% xyo-cloud
 
 call build.%ACTION%.cmd
